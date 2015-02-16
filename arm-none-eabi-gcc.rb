3306398b1ba7a5175e4c01956961ee4d6659b843
Kunihiro AIHARA <rohiniku@users.noreply.github.com>
2015-02-17 00:25:57 +0900

    Update arm-none-eabi-gcc.rb

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -5,7 +5,7 @@ class ArmNoneEabiGcc < Formula
   homepage 'https://launchpad.net/gcc-arm-embedded'
   version '20140609'
   url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2014-q4-major/+download/gcc-arm-none-eabi-4_9-2014q4-20141203-mac.tar.bz2'
-  md5 'a3b0ab5bd08ba5ad840b7cb5f17becb0'
+  sha1 '46767b358cc42ffe126aec68afb9d22bd0ef5beb'
   
   def install
     system 'cp', '-rv', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
