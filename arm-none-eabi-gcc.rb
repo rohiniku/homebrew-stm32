c7b65b93fa2240ad1f511f29f1dea8081433a4df
Kunihiro AIHARA <aihara.kunihiro@gmail.com>
2015-05-24 02:37:12 +0900

    Update arm-none-eabi-gcc version 4.9-2014-q4-major to 4.9-2015-q1-major

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -4,7 +4,7 @@ class ArmNoneEabiGcc < Formula
 
   homepage 'https://launchpad.net/gcc-arm-embedded'
   version '20140609'
-  url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2014-q4-major/+download/gcc-arm-none-eabi-4_9-2014q4-20141203-mac.tar.bz2'
+  url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-major/+download/gcc-arm-none-eabi-4_9-2015q1-20150306-mac.tar.bz2'
   sha1 '46767b358cc42ffe126aec68afb9d22bd0ef5beb'
   
   def install
