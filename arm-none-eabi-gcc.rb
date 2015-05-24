a086aef0cd3d5b0f32fae02ccf4eb0cadb29b8ba
Kunihiro AIHARA <aihara.kunihiro@gmail.com>
2015-05-24 10:53:56 +0900

    Fix download URL

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -4,9 +4,9 @@ class ArmNoneEabiGcc < Formula
 
   homepage 'https://launchpad.net/gcc-arm-embedded'
   version '20150306'
-  url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-major/+download/gcc-arm-none-eabi-4_9-2015q1-20150306-mac.tar.bz2'
+  url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-update/+download/gcc-arm-none-eabi-4_9-2015q1-20150306-mac.tar.bz2'
   sha1 '46767b358cc42ffe126aec68afb9d22bd0ef5beb'
-  
+
   def install
     system 'cp', '-rv', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
   end
