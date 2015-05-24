c191e97919745a1efa99c5007c01f374437d6ee3
Kunihiro AIHARA <aihara.kunihiro@gmail.com>
2015-05-24 10:39:27 +0900

    Update arm-none-eabi-gcc version 20140609 to 20150306

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ArmNoneEabiGcc < Formula
 
   homepage 'https://launchpad.net/gcc-arm-embedded'
-  version '20140609'
+  version '20150306'
   url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-major/+download/gcc-arm-none-eabi-4_9-2015q1-20150306-mac.tar.bz2'
   sha1 '46767b358cc42ffe126aec68afb9d22bd0ef5beb'
   
